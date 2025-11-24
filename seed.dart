b2111c1518d19ec854ea0e6cddc493ab0eae34a8
Daniel Shinji <145377796+DShinji09@users.noreply.github.com>
2025-11-24 00:01:20 -0300

    Update seed.dart

--- seed.dart
+++ seed.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_print
 
-import 'dart:math';
+import 'dart:math'
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
