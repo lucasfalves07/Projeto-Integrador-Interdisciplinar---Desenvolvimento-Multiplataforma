aad89b1add6d0dee7d215cf2b40d44cf8dd97f6b
Daniel Shinji <145377796+DShinji09@users.noreply.github.com>
2025-11-24 00:01:53 -0300

    Update seed.dart

--- seed.dart
+++ seed.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'lib/firebase_options.dart'
+import 'lib/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
